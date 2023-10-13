15834358e3165a92f611919a86aeb1bbbd2df447
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-10-13 15:48:32 -0700

    js and .ds_store changes

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const jobRoleMenu = document.querySelector('#title');
 const otherJobInput = document.querySelector('#other-job-role');
 otherJobInput.hidden = true;
 
-jobRoleMenu.addEventListener('change', (e)=>{
-    if(e.target.value === 'other'){
+jobRoleMenu.addEventListener('change', (e) => {
+    if (e.target.value === 'other') {
         otherJobInput.style.display = 'block';
-    }else{
-        otherJobInput.style.display = 'none'; 
+    } else {
+        otherJobInput.style.display = 'none';
     }
 });
 
@@ -21,34 +21,34 @@ shirtColor.disabled = true;
 const shirtDesign = document.querySelector('#design');
 const shirtColorOptions = shirtColor.querySelectorAll('option[data-theme]');
 
-shirtDesign.addEventListener('change', (e)=>{
+shirtDesign.addEventListener('change', (e) => {
     shirtColor.disabled = false;
-    for(let i=0; i<shirtColorOptions.length; i++){
+    for (let i = 0; i < shirtColorOptions.length; i++) {
         const shirtColors = shirtColorOptions[i];
-        if(e.target.value !== shirtColors.dataset.theme){
+        if (e.target.value !== shirtColors.dataset.theme) {
             const firstOption = document.querySelector(`[data-theme = "${e.target.value}"]`);
             firstOption.selected = true;
             shirtColors.style.display = 'none';
-        }else{
+        } else {
             shirtColors.style.display = 'block';
         }
     }
-    
+
 });
 
 //#5 ACTIVITIES SECTION: allows users to register for activities and see their real-time updated balance. Extra credit was added as well to disabled events that are during conflicting dates and times
 const activities = document.querySelector('#activities');
 let balance = 0;
 
-activities.addEventListener('change', (e)=>{
+activities.addEventListener('change', (e) => {
     const selected = e.target;
     //Total Activities Cost
     const price = parseInt(selected.dataset.cost);
     const checked = selected.checked;
-    if(checked){
-        balance+=price;
-    }else{
-        balance-=price;
+    if (checked) {
+        balance += price;
+    } else {
+        balance -= price;
     }
     const activitiesBalance = document.querySelector('#activities-cost');
     activitiesBalance.innerHTML = `Total: $${balance}`;
@@ -56,13 +56,13 @@ activities.addEventListener('change', (e)=>{
     //CONFLICTING ACTIVITY TIMES - conflicting events that are at the same date and time are disabled when selected
     const selectedDayAndTime = selected.dataset.dayAndTime;
     const sameDayAndTime = document.querySelectorAll(`[data-day-and-time = "${selectedDayAndTime}"]`);
-    for(let i=0; i<sameDayAndTime.length; i++){
+    for (let i = 0; i < sameDayAndTime.length; i++) {
         const conflictingEvents = sameDayAndTime[i];
         conflictingEvents.disabled = selected.checked && conflictingEvents !== selected;
-        if(conflictingEvents.disabled){
+        if (conflictingEvents.disabled) {
             conflictingEvents.parentElement.className = 'disabled';
-        }else{
-            conflictingEvents.parentElement.className = ''; 
+        } else {
+            conflictingEvents.parentElement.className = '';
         }
     }
 });
@@ -77,19 +77,19 @@ const bitcoin = document.querySelector('#bitcoin');
 
 //default payment view
 payment.children[1].setAttribute('selected', 'selected');
-paypal.hidden= true;
+paypal.hidden = true;
 bitcoin.hidden = true;
 //hide and show payment options
-payment.addEventListener('change', (e)=>{
-    if(e.target.value === 'paypal'){
+payment.addEventListener('change', (e) => {
+    if (e.target.value === 'paypal') {
         paypal.hidden = false;
         creditCard.hidden = true;
         bitcoin.hidden = true;
-    }else if(e.target.value ==='bitcoin'){
+    } else if (e.target.value === 'bitcoin') {
         paypal.hidden = true;
-        creditCard.hidden= true;
+        creditCard.hidden = true;
         bitcoin.hidden = false;
-    }else{
+    } else {
         paypal.hidden = true;
         creditCard.hidden = false;
         bitcoin.hidden = true;
@@ -100,7 +100,7 @@ payment.addEventListener('change', (e)=>{
 //#7 FORM VALIDATION: name, email, activities, credit card number, zipcode, and cvv are all validated both to prevent the user from submitting the form if incorrect/incomplete and to show them their errors
 const form = document.querySelector('form');
 const emailInput = document.querySelector('#email');
-const activitiesBox =  document.querySelector('#activities-box');
+const activitiesBox = document.querySelector('#activities-box');
 const activitySelections = activitiesBox.querySelectorAll('input');
 const ccNum = document.querySelector('#cc-num');
 const zipCodeNum = document.querySelector('#zip');
@@ -112,7 +112,7 @@ const cvvNum = document.querySelector('#cvv');
  * @returns boolean
  */
 
-function nameValidation(){
+function nameValidation() {
     const nameValue = nameInput.value;
     const nameRegex = /\w+/;
     return nameRegex.test(nameValue);
@@ -124,7 +124,7 @@ function nameValidation(){
  * @returns boolean
  */
 
-function emailValidation(){
+function emailValidation() {
     const emailValue = emailInput.value;
     const emailRegex = /^[^@]+@[^@.]+\.[a-z]+$/i;
     return emailRegex.test(emailValue);
@@ -136,18 +136,18 @@ function emailValidation(){
  * @returns boolean
  */
 
-function activitiesValidation(){
-    const activitySelectionsChecked =[];
-    for(let i=0; i<activitySelections.length; i++){
+function activitiesValidation() {
+    const activitySelectionsChecked = [];
+    for (let i = 0; i < activitySelections.length; i++) {
         const activitiesSelected = activitySelections[i]
         const checked = activitiesSelected.checked;
-        if(checked){
+        if (checked) {
             activitySelectionsChecked.push(activitiesSelected);
-        } 
+        }
     }
-    if(activitySelectionsChecked.length>=1){
+    if (activitySelectionsChecked.length >= 1) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
@@ -158,12 +158,12 @@ function activitiesValidation(){
  * @returns boolean
  */
 
-function cardNumberValidation(){
-    if(payment.value === 'credit-card'){
+function cardNumberValidation() {
+    if (payment.value === 'credit-card') {
         const cardNumRegex = /^\d{13,16}$/;
         const cardNumInput = ccNum.value;
         return cardNumRegex.test(cardNumInput);
-    }else{
+    } else {
         return true;
     }
 }
@@ -174,12 +174,12 @@ function cardNumberValidation(){
  * @param none
  * @returns boolean
  */
-function zipCodeValidation(){
-    if(payment.value === 'credit-card'){
+function zipCodeValidation() {
+    if (payment.value === 'credit-card') {
         const zipCodeRegex = /^\d{5}$/;
         const zipCodeInput = zipCodeNum.value;
         return zipCodeRegex.test(zipCodeInput);
-    }else{
+    } else {
         return true;
     }
 }
@@ -190,29 +190,29 @@ function zipCodeValidation(){
  * @returns boolean
  */
 
-function cvvValidation(){
-    if(payment.value === 'credit-card'){
+function cvvValidation() {
+    if (payment.value === 'credit-card') {
         const cvvRegex = /^\d{3}$/;
         const cvvInput = cvvNum.value;
         return cvvRegex.test(cvvInput);
-    }else{
+    } else {
         return true;
     }
 }
 
 //#8 - ACCESSIBILITY
-for(let i=0; i<activitySelections.length; i++){
+for (let i = 0; i < activitySelections.length; i++) {
     const activitiesSelected = activitySelections[i];
-    activitiesSelected.addEventListener('focus', ()=>{
+    activitiesSelected.addEventListener('focus', () => {
         activitiesSelected.parentElement.className = 'focus';
     });
-    activitiesSelected.addEventListener('blur', ()=>{
+    activitiesSelected.addEventListener('blur', () => {
         const focus = activitiesBox.querySelectorAll('.focus');
-        for(let i=0; i<focus.length; i++){
+        for (let i = 0; i < focus.length; i++) {
             focus[i].removeAttribute('class');
         }
-        
-    }); 
+
+    });
 }
 
 //#9 VISUAL VALIDATION ERRORS
@@ -221,16 +221,16 @@ for(let i=0; i<activitySelections.length; i++){
  * @param {variable} validator - takes in the variable that stores the element you want to focus on
  * @param {function} validatorFunc - takes the validator function associated with the focus element variable 
  */
-function displayErrors(validator, validatorFunc){
+function displayErrors(validator, validatorFunc) {
     const selector = validator;
     const selectorParent = selector.parentElement;
     const selectorHint = selectorParent.querySelector('.hint');
 
-    if(!validatorFunc()){
+    if (!validatorFunc()) {
         selectorParent.classList.add('not-valid');
         selectorParent.classList.remove('valid');
         selectorHint.style.display = 'block';
-    }else{
+    } else {
         selectorParent.classList.add('valid');
         selectorParent.classList.remove('not-valid');
         selectorHint.style.display = 'none';
@@ -243,12 +243,12 @@ function displayErrors(validator, validatorFunc){
  * runs a conditional
  */
 
-function emailDisplayError(){
-    if(emailInput.value === ''){
+function emailDisplayError() {
+    if (emailInput.value === '') {
         const emailHint = document.querySelector('#email-hint');
         emailHint.textContent = 'Email address field cannot be blank';
         displayErrors(emailInput, emailValidation);
-    }else{
+    } else {
         const emailHint = document.querySelector('#email-hint');
         emailHint.textContent = 'Email address must be formatted correctly';
         displayErrors(emailInput, emailValidation);
@@ -256,27 +256,27 @@ function emailDisplayError(){
 }
 
 //REAL TIME VALIDATION ERRORS on blur or click events
-nameInput.addEventListener('blur', ()=>{
+nameInput.addEventListener('blur', () => {
     displayErrors(nameInput, nameValidation);
 });
 
-emailInput.addEventListener('blur', ()=>{
+emailInput.addEventListener('blur', () => {
     emailDisplayError();
 });
 
-activitiesBox.addEventListener('click', ()=>{
+activitiesBox.addEventListener('click', () => {
     displayErrors(activitiesBox, activitiesValidation);
 });
 
-ccNum.addEventListener('blur', ()=>{
+ccNum.addEventListener('blur', () => {
     displayErrors(ccNum, cardNumberValidation);
 });
 
-zipCodeNum.addEventListener('blur', ()=>{
+zipCodeNum.addEventListener('blur', () => {
     displayErrors(zipCodeNum, zipCodeValidation);
 });
 
-cvvNum.addEventListener('blur', ()=>{
+cvvNum.addEventListener('blur', () => {
     displayErrors(cvvNum, cvvValidation);
 });
 
@@ -285,15 +285,15 @@ cvvNum.addEventListener('blur', ()=>{
 /**
  * Final event listener to listen for errors when the form is submitted
  */
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     const name = nameValidation();
     const email = emailValidation();
     const activities = activitiesValidation();
     const cardNumber = cardNumberValidation();
     const zipcode = zipCodeValidation();
     const cvv = cvvValidation();
-    
-    if(!name || !email || !activities || !cardNumber || !zipcode || !cvv){
+
+    if (!name || !email || !activities || !cardNumber || !zipcode || !cvv) {
         e.preventDefault();
         displayErrors(nameInput, nameValidation);
         emailDisplayError();
